2cf5c1794e28184cf7756176009bc2583cb6cbc5
nathanshuai <nathan.shuai@gmail.com>
2023-03-02 11:25:51 -0600

    addmenu

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
+const menuIcon = document.querySelector('.icon.one');
+const dropdownMenu = document.querySelector('.headerlist');
+
+console.log(menuIcon);
+
+menuIcon.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('show');
+
+});
 
